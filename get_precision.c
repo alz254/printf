c96563b1db2ece97f85e86aa714628d0146ba009
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-07-11 16:57:43 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,43 +1,51 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed
- * @list: list of arguments
+ * handle_precision - Matches a precision modifier with
+ *                    its corresponding value.
+ * @args: A va_list of arguments.
+ * @modifier: A pointer to a potential precision modifier.
+ * @index: An index counter for the original format string.
  *
- * Return: Precision.
+ * Return: If a precision modifier is matched - its value.
+ *         If the precision modifier is empty, zero, or negative - 0.
+ *         Otherwise - -1.
  */
-int get_precision(const char *format, int *i, va_list args)
+int handle_precision(va_list args, const char *modifier, char *index)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+	int value = 0;
 
-	if (format[curr_i] != '.')
+	if (*modifier != '.')
+		return (-1);
+
+	modifier++;
+	(*index)++;
+
+	if ((*modifier <= '0' || *modifier > '9') &&
+	     *modifier != '*')
 	{
-		return (precision);
+		if (*modifier == '0')
+			(*index)++;
+		return (0);
 	}
 
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	while ((*modifier >= '0' && *modifier <= '9') ||
+	       (*modifier == '*'))
 	{
-		if (is_digit(format[curr_i]))
+		(*index)++;
+
+		if (*modifier == '*')
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
+			value = va_arg(args, int);
+			if (value <= 0)
+				return (0);
+			return (value);
 		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(args, int);
-			break;
-		}
-		else
-			break;
+
+		value *= 10;
+		value += (*modifier - '0');
+		modifier++;
 	}
 
-	*i = curr_i - 1;
-
-	return (precision);
+	return (value);
 }
