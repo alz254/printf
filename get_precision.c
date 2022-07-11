26abb88060cdee5f4fb2199c52c095ed41c50c26
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-07-11 16:49:37 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -8,7 +8,7 @@
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_arg list)
+int get_precision(const char *format, int *i, va_list args)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
@@ -30,7 +30,7 @@ int get_precision(const char *format, int *i, va_arg list)
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			precision = va_arg(list, int);
+			precision = va_arg(args, int);
 			break;
 		}
 		else
