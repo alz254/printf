7241798c93ac31cd21bcc9372d793e32f0f73eb4
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-07-12 17:05:32 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -24,11 +24,13 @@ int get_precision(const char *format, int *i, va_list list)
 		{
 			precision *= 10;
 			precision += format[curr_i] - '0';
+				return (0);
 		}
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
 			precision = va_arg(list, int);
+				return (0);
 			break;
 		}
 		else
